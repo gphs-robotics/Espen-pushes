d7fb5a8375c9de2662eb515eb413f8aba78b4542
espen <espenrhonebaker@gmail.com>
2025-12-16 21:58:57 +0000

    initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+//copy and paste to run: javac src/App.java && java -cp src App
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
